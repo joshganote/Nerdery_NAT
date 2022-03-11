04430265b9558b9acfe72ac2e2796f08e69685c9
joshganote <j.ganote0825@gmail.com>
2022-03-10 21:11:19 -0600

    wanted to use styled components but decided to use pure css for now and do that that later. Initial setup for boxes done

--- src/components/SnackVote/SnackVote.js
+++ src/components/SnackVote/SnackVote.js
@@ -1,23 +1,42 @@
 import React from "react";
-import {useFetchSnacks} from "../../api/useSnacks";
-import "./SnackVote";
+import { useFetchSnacks } from "../../api/useSnacks";
+import {
+  CurrentSnackContainer,
+  CurrentSnackImg,
+  CurrentSnackBox,
+} from "../SnackVote/SnackVoteStyled";
 
-export const SnackVote =() => {
-  const {data, error} = useFetchSnacks();
+import "./snack.css";
+
+export const SnackVote = (props) => {
+  const { data, error } = useFetchSnacks();
   console.log(`Error: ${error}`);
 
   return (
-    <div>
+    <div className="snack-container">
       <h1>SnackVote</h1>
       {data.map((item, index) => (
-        <div key={index}>
-        <p>{item.brand}</p>
-        <p>{item.description}</p>
-        <p>{item.product}</p>
-        <p>{item.votes}</p>
-        <img src={item.image} alt={item.product}/>
+        // <CurrentSnackContainer>
+        //   <CurrentSnackBox>
+
+        /* <p>{item.brand}</p>
+            <p>{item.description}</p>
+            <p>{item.product}</p>
+            <p>{item.votes}</p> */
+        <div className="snack-box">
+
+          <div className="vote-corner">
+            <p className="vote-number">{item.votes}</p>
+          </div>
+
+          <div className="img-container">
+            <div className="img-inner">
+              <img className="image" src={item.image} alt={item.product} />
+            </div>
+          </div>
+
         </div>
       ))}
     </div>
   );
-}
+};
